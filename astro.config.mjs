9972796f381fa3964de3c54dcd244561dd5f0109
yuxxeun <itsyuxxeun@gmail.com>
2023-04-17 15:34:35 +0700

    feat: astro built-in  integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import svelte from "@astrojs/svelte";
+import compress from "astro-compress";
+import serviceWorker from "astrojs-service-worker";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  site: "https://veroniqueorocorp.com",
+  output: "static",
   adapter: vercel({
     analytics: true
   }),
-  integrations: [tailwind(), sitemap(), prefetch(), svelte()]
+  integrations: [
+    tailwind(),
+    sitemap(),
+    prefetch(),
+    svelte(),
+    compress(),
+    serviceWorker(),
+    astroImageTools
+  ]
 });
---
+++ src/config.mjs
@@ -0,0 +1,9 @@
+export const SITE = {
+  title: "Edgy",
+  description: "The top layer Veronique homebase",
+  homebase: "https://veroniqueorocorp.com",
+  basePath: "/",
+  github: "https://github.com/wearevero",
+  imageBanner:
+    "https://raw.githubusercontent.com/wearevero/.github/main/team/basement.jpeg"
+};
