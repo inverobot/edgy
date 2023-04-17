bd6fae6a9c3326645c94949eec64289fadb61db7
yuxxeun <itsyuxxeun@gmail.com>
2023-04-17 11:49:43 +0700

    initialize edgy project'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     analytics: true
   }),
